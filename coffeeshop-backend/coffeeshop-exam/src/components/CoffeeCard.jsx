1b2a462bb270d462fcb0298e8707210ae75201c6
Tibi81 <125083407+Tibi81@users.noreply.github.com>
2025-01-25 21:03:46 +0100

    Update CoffeeCard.jsx

--- coffeeshop-backend/coffeeshop-exam/src/components/CoffeeCard.jsx
+++ coffeeshop-backend/coffeeshop-exam/src/components/CoffeeCard.jsx
@@ -3,7 +3,12 @@ import "./css/CoffeeCard.css";
 
 function CoffeeCard({ coffee }) {
   return (
-    <div className="coffee-card" ></div>);
+    <div className="coffee-card" >
+      <h2>{coffee.name}</h2>
+      <p>{coffee.description ? coffee.description : "No description available."}</p>
+      <p><strong>Weight:</strong> {coffee.amount_in_grams}g</p>
+      <p><strong>Price:</strong> ${coffee.price}</p>
+    </div>);
 }
 
 export default CoffeeCard;
